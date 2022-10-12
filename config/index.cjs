01dab2bc57589022817b9bd6365188f50200021d
Rich-King-Lear <115631331+Rich-King-Lear@users.noreply.github.com>
2022-10-12 19:32:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ovChz6so-fpW4uXFnEH7Ld-9lHeY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'C6NKdGk1tUOm2FJ69RwEefVVY-N5SZK7NYLA6ku9bLg',
+      useTemplateId: 'kQrlUX_39QiLU-7liarysA9VH_sLKI5ThEjf-JXBaC0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-16',
       festivals: [
