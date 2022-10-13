b1410d7aa48f72df6c38b44cf3ba695760aa0995
Li shengjie <115631331+Rich-King-Lear@users.noreply.github.com>
2022-10-13 08:53:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,6 +22,7 @@ const USER_CONFIG = {
       name: '老婆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ovChz6rFxExFzXnvpTs-oQqol3R4',
+      id: 'ovChz6so-fpW4uXFnEH7Ld-9lHeY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'ObfmduQSHX23sUxRc659wdUBJbrBfUy3aHnqNZLLAjY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
